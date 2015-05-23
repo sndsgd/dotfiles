d9369e4ac1a3150de567c064ec76cd53e44f2787
Russell <r@snds.gd>
2015-05-23 13:19:03 -0400

    updates colors an adds inject instructions

--- README.md
+++ README.md
@@ -2,9 +2,23 @@
 
 Loosely based on [Mathias Bynens'](https://github.com/mathiasbynens/dotfiles) dotfiles.
 
-## Usage
 
-```bash
-git clone https://github.com/sndsgd/dotfiles.git
-dotfiles/update.sh --local && source ~/.bash_profile
+
+## Install
+
+Copy and paste the following into your terminal and hit enter
+
+```sh
+inject_dotfiles() {
+   local CURRENT_DIR=$(pwd)
+   local URL=https://github.com/sndsgd/dotfiles/archive/master.tar.gz
+   curl -L -s -o /tmp/dotfiles.tgz $URL
+   tar -xvzf /tmp/dotfiles.tgz -C /tmp
+   rm /tmp/dotfiles.tgz
+   source /tmp/dotfiles-master/update.sh --local
+   rm -rf /tmp/dotfiles-master
+}
+inject_dotfiles && \
+unset inject_dotfiles
 ```
+
--- update.sh
+++ update.sh
@@ -1,35 +1,53 @@
 #!/bin/bash
 
+OS_NAME=$(uname)
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if [ "$1" != "--local" ] && [ "$1" != "-l" ]; then
-   echo "updating to latest version..."
-   git --git-dir="$DIR/.git" --work-tree="$DIR" pull
-fi
 
-echo "copying files..."
-rsync --exclude ".DS_Store" --archive --quiet "$DIR/files/." ~
-
+create_git_dir() {
+   if [ ! -d "$1" ]; then
+      echo "creating git directory in '$1'"
+      mkdir -pv "$1"
+   fi
+}
 
 link_ssh_config() {
-   local sshdir="$HOME/.ssh"
-   local destpath="$sshdir/config"
-   local sourcepath="$1"
-   if [ ! -f "$destpath" ] && [ -f "$sourcepath" ]; then
-      echo "creating link to ssh config in $sourcepath"
-      if [ ! -d "$sshdir" ]; then
-         mkdir -pv "$sshdir"
-      fi
-      ln -s "$sourcepath" "$destpath"
-      chmod 600 "$destpath"
+   local DEST_PATH="$HOME/.ssh/config"
+   local SOURCE_PATH="$1"
+   if [ ! -f "$DEST_PATH" ] && [ -f "$SOURCE_PATH" ]; then
+      echo "creating link to ssh config in $SOURCE_PATH"
+      ln -s "$SOURCE_PATH" "$DEST_PATH"
+      chmod 600 "$DEST_PATH"
    fi
 }
 
 
-echo "updating bash environment..."
-source ~/.bash_profile
+# setup tasks ###############################################################
+#############################################################################
 
 OS_NAME=$(uname)
+
+[ ! -d "$HOME/.ssh" ] && mkdir -pv "$HOME/.ssh"
+
 if [ "$OS_NAME" = "Darwin" ]; then
+   create_git_dir "$HOME/Documents/git"
    link_ssh_config "$HOME/Dropbox/Documents/ssh/config"
+elif [ "$OS_NAME" = "Linux" ]; then
+   create_git_dir "$HOME/git"
 fi
+
+
+# copy files into $HOME #####################################################
+#############################################################################
+
+echo "copying files..."
+rsync --exclude ".DS_Store" --archive --quiet "$DIR/files/." "$HOME"
+
+
+# cleanup ###################################################################
+#############################################################################
+
+unset OS_NAME
+unset DIR
+unset create_git_dir
+unset link_ssh_config

b5bb47f3787242286ec5126719b25562e97a5a5e
Russell Barnhart <russell@tumblr.com>
2017-08-28 11:42:55 -0400

    remove homebrew php things

--- bash/path.sh
+++ bash/path.sh
@@ -1,4 +1,4 @@
 
-[ -d "$HOME/bin" ] && PATH="$HOME/bin:$PATH"
 [ -d /usr/local/sbin ] && PATH="/usr/local/sbin:$PATH"
-
+[ -d "$HOME/.composer/vendor/bin" ] && PATH="$HOME/.composer/vendor/bin:$PATH"
+[ -d "$HOME/bin" ] && PATH="$HOME/bin:$PATH"
